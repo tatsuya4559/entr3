45a2e2efb1eccfbe4aaa4281773ae19e47385d06
tatsuya4559 <tatsuya.k1029@gmail.com>
2023-12-16 17:29:27 +0900

    setup signal handler

--- eptr.c
+++ eptr.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <err.h>
 
 static void print_usage(char *cmd) {
   fprintf(stderr, "%s utility [arguments...]\n", cmd);
+}
+
+static void handle_exit(int sig) {
+  // TODO: terminate utility
+  if (sig == SIGINT || sig == SIGHUP) {
+    _exit(0);
+  }
+  raise(sig);
+}
+
+static void setup_signal_handler(void) {
+  struct sigaction act;
+  sigemptyset(&act.sa_mask);
+  act.sa_flags = SA_RESETHAND;
+  act.sa_handler = handle_exit;
+  if (sigaction(SIGINT, &act, NULL) != 0) {
+    err(EXIT_FAILURE, "Failed to set SIGINT handler");
+  }
+  if (sigaction(SIGTERM, &act, NULL) != 0) {
+    err(EXIT_FAILURE, "Failed to set SIGTERM handler");
+  }
+  if (sigaction(SIGHUP, &act, NULL) != 0) {
+    err(EXIT_FAILURE, "Failed to set SIGHUP handler");
+  }
 }
 
 int main(int argc, char **argv) {
@@ -10,6 +37,8 @@ int main(int argc, char **argv) {
     print_usage(argv[0]);
     return EXIT_FAILURE;
   }
+
+  setup_signal_handler();
 
   return EXIT_SUCCESS;
 }
